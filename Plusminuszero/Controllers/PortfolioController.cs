3a952a754a8bc585f4647c6534bfe7852ef6468f
Yumeno198271 <61045103+Yumeno198271@users.noreply.github.com>
2020-07-12 20:35:47 +0800

    11111

---
+++ Plusminuszero/Controllers/PortfolioController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plusminuszero.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Portfolio_Masonry()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Portfolio_Dribbble()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Welfare_Goods()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Dribbble()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        public ActionResult Portfolio_2column()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Portfolio_3column()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
