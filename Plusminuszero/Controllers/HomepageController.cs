89182e15a67305df4d31bc91b460c2d81a69999a
Yumeno198271 <61045103+Yumeno198271@users.noreply.github.com>
2020-07-12 20:05:12 +0800

    20200711-2

---
+++ Plusminuszero/Controllers/HomepageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plusminuszero.Controllers
+{
+    public class HomepageController : Controller
+    {
+        // GET: Homepage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Homepage_Architecture()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Slider_Full_Width()
+        {
+            return View();
+        }
+
+
+
+
+
+        public ActionResult Single_Project()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Plusminuszero/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plusminuszero.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Event_News()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Product_Information()
+        {
+            return View();
+        }
+
+
+        public ActionResult Blog_Single()
+        {
+            return View();
+        }
+
+
+    }
+}
