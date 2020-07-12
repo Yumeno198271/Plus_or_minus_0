c5410cb8678ee2cff1fd78d210cdd621cfe4d540
Yumeno198271 <61045103+Yumeno198271@users.noreply.github.com>
2020-07-12 21:04:31 +0800

    123

---
+++ Plusminuszero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plusminuszero.Startup))]
+namespace Plusminuszero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
