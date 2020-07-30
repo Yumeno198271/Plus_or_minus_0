4efdffdc7508ec665c10f1f5d1d5439ccfce9a81
Yumeno198271 <61045103+Yumeno198271@users.noreply.github.com>
2020-07-30 14:42:49 +0800

    12345

---
+++ Plusminuszero/Controllers/MemberManagementController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plusminuszero.Controllers
+{
+    public class MemberManagementController : Controller
+    {
+        // GET: MemberManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Backstage_Home()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Plusminuszero/Models/AspNetUsers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plusminuszero.Models
+{
+    public class AspNetUsers
+    {
+
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
